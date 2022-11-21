e43f8103b13d457c1a5dbeef868059352a82868f
sohailkhan0550 <hunzaisohail71@gmail.com>
2022-11-21 13:31:22 +0500

    ASAS

---
+++ js/index.js
@@ -0,0 +1,408 @@
+
+
+// var a = []
+// var b = []
+
+// console.log(a[0] == b[0])
+//  debugger
+// var year = 2020;
+// if (year % 4 == 0) {
+//     if (year % 9 == 0){
+//     if (year % 100 == 0) {
+//         {
+//             console.log('this is a leap year')
+
+//         }
+//     }else{
+//         console.log('this is a leap year')
+
+//     }
+
+// }else{
+//     console.log('this is not leap year')
+// // }
+// let per = prompt("what is tour percentage---?");
+
+// if (per >= 55) {
+//  console.log("you can eleigble for admissin")
+// }else{
+//  console.log("Sorryyy you can NOT eleigble for admissin")
+ 
+// }
+
+// var age = 2;
+// console.log( (age > 17) ? "you are eleible to vote": "you are not elegible to vote")
+// var area = "circle";
+// var PI = 3.143, l=5, b= 4, r=3;
+// if (area == "circle") {     
+//   console.log("the area of the circle is: " + PI*r**2);      
+// }else if(area = "triangle"){
+//     console.log("the area of the circle is: " + PI*r**2);      
+
+// }else if(
+// console.log("the area of the triange is : + "(l*b))
+// )
+
+
+
+// var area = 'triangle';
+//  pi = 3.143,l=5, b= 4, r=3;
+
+//  if (area == 'circle') {
+//     console.log("the area of circle is" + pi*r**2);
+//  }else if(area == 'triangle'){
+//     console.log("the area of regtangle is" + (l*b)/2);
+
+//  }else if(area == 'regtangle'){
+//     console.log("the area of tiangle is" + (l*b));
+
+    
+//  }else{
+//     console.log("plese invalid");
+
+//  }
+
+// SWITCH STATEMENT
+
+// var area = "circle";
+//  var pi = 3.143,l=5, b= 4, r=3;
+
+//  switch(area){
+//     case 'circle' :
+//         console.log('the value of circle is' + pi*r**2);
+//     case 'triangle':
+//         console.log('the value of circle is' + pi);
+     
+//  }
+//   var num = 10;
+// //  block scop
+//  while(num <=  20 ){
+//     console.log(num);
+//     num++;
+// //  }
+//    var note = 0;
+//  do{
+//    console.log(note);
+//    note++;
+//  }while(note < 10);
+
+// var flim = 0;
+// debugger
+// do{
+//    console.log(flim);
+//    flim++;
+// }while(flim < 10);
+
+
+
+// var a = 0;
+// while(a <= 10){
+//    console.log(a)
+//     a++;
+// }
+
+// do{
+//   console.log(a);
+//   a++;
+// }while(a < 10);
+// var book = 20;
+
+// for(var num = 1; num <= 10; num++){
+//      var tableof = 8;
+//      console.log(tableof + " * " + num + "=" + tableof * num );   
+// }
+// var tableof = 9;
+// for(var num = 1; num <= 10; num++){
+//     console.log(tableof + "*" + num + "=" + tableof * num )
+// }
+// var pi = 3.14;
+//   debugger
+//  for(var num = 1;num <= 10; num++){
+//     // count = 20;
+//     console.log(count + "*" + num + "=" + pi * num)
+//  }
+// algorithm///
+
+//  var find = [];
+//  var finds = [];
+//  console.log(find[0] == finds[0]);
+
+
+// switch("kkk"){
+//     case "circle" :
+//         console.log('the area of circle is 10ssss');
+//         break;
+//     case "trinangel" :
+//         console.log('the area of circle is 10');
+//       default:
+//         console.log("please enter valid data")
+// }
+
+//  var a = 0;
+
+//  while(a <= 10){
+//    console.log(a + "=" +"this is the value");
+//    a++;
+//  }
+
+// var num = 0;
+
+// while(num <= 10){
+//     console.log(num)
+//     num++;
+// }
+// do{ 
+//  console.log(num)
+//  num++;
+// }while(num <= 10);
+// var ar = 1;
+
+// while(ar <= 10){
+//  var  s = 8;
+//     console.log(s + "*" + ar + "=" + ar * s);
+//       ar++;
+// }
+
+// function book(){
+//  var a = 10; b = 5;
+//   var total  = a+b;
+//   console.log(total)
+// }
+// book()
+
+// function vv(){
+//   var a = 10; b = 5;
+//   var total =  a+b;
+//   console.log(total);
+// }
+
+
+//  for(var num = 1;num < 10; num++){
+//   var table = 8;
+//   console.log(table + "*" + num + "=" + num * table)
+//  }
+
+// function num(a,b){
+//   const fun = (a+b)
+//   console.log(fun);
+// }
+
+// num(20,10);
+  
+
+// var a = [];
+// var b = [];
+ 
+// console.log(a[0] == b[0])
+
+//   function sum(a,b){
+//    return total = a+b;    
+//   }
+
+//   var txx = sum(20,10);
+//     console.log("your sum value is" + "=" + txx)
+
+
+
+
+
+
+
+// function sum(){
+//   var sux
+// }
+
+// function sum(a,b){
+//   return total = a+b;
+// }
+
+// var fun = sum(50,100);
+// var funz = sum(50,10);
+// console.log(fun)
+// console.log(funz)
+
+
+
+// function sum(a,b){
+//   return total = a+b;
+// }
+// var fix1 = sum(20,10);
+// var fix2 = sum(200,20);
+// console.log(fix1 < fix2)
+ 
+//  function datas(){
+//   var myfirstname = "sohail";
+//   console.log(myfirstname);
+//   if(true){
+//     var mylastname = "sohail";
+//     console.log('inner' + mylastname);
+//     console.log('inner' + myfirstname);
+//   }
+//   console.log('innerouter' + mylastname)
+//  }
+//   datas();
+//  debugger
+// function num(a,b){
+//   var sys = a+b;
+//   console.log(sys)
+
+// }
+
+// num(20,10);
+// num(10,19);
+
+// function num(a,b){
+//    return total = a+b;
+// }
+
+// var funexp = sum(5,10);
+// console.log('the sum of two num iss ' + funexp)
+
+// var funexp = function(a,b){
+//   return total = a+b;
+
+// }
+
+// var sum1 = funexp(10,30); 
+// var sum2 = funexp(20,40);
+
+// console.log(sum1 < sum2);
+ 
+// function book(){
+  
+// }
+
+// function show(){
+//   alert('books')
+//   alert('looks')
+// }
+// show()   
+// show()   
+// function showMessage() {
+//   let message = "Hello, I'm JavaScript!"; // local variable
+//   alert( message );
+//   }
+//   showMessage(); // Hello, I'm JavaScript!
+//   alert( message ); // <-- Error! The variable is local to the function
+  
+// function showMessage(){
+//   let mesg = "hello sohail" //this is local variable
+//   alert(mesg)
+
+// }
+
+// showMessage();
+// var hello = "this is javasscript"
+
+// function show(){
+//   var hello = "this is javasscript"
+//   alert(hello)
+// }
+// show();
+
+//   let newww = "sohal";
+
+// function shows(){
+//   var blue = "this outer var";
+//   alert(helloo);
+// }
+
+// shows();
+// ////////////////////
+
+// let userName = 'John';
+// function showMessage() {
+// let message = 'Hello, ' + userName;
+// alert(message);
+// }
+
+// showMessage()
+// var a = [];
+// var b = [];
+
+// if (a[0] == b[0]) {
+//   alert("this is not true")
+// }else{
+//   alert("this is not true")
+// }
+
+// function ShowMessage(firstName, lastName) {
+//   alert("Hello " + firstName + " " + lastName);
+// }
+
+// ShowMessage("Steve", "sohail");
+// ShowMessage("Bill", "Gatesszzzzzz");
+// ShowMessage(100, 200);
+
+// function ShowMessage(firstName, lastName) {
+//   alert("Hello " + firstName + " " + lastName);
+// }
+
+// ShowMessage("Steve", "sohail");
+// ShowMessage("Bill", "Gatesszzzzzz");
+// ShowMessage(100, 20);
+
+//    function showmesg(firstname){
+//   alert('hello world')
+//  }
+ 
+// var cx = 2;
+// var cx = 2;
+
+// if (cx == 2) {
+//       console.log("sohail")
+//     }else{
+//       console.log("sohail not")
+
+//     }
+
+//    function add(a,b){
+//     var cx = 10 + 20;
+//     console.log(cx)
+//    }
+//    add();
+//    add();
+
+  // function roof(){
+  //   alert('sohaail');
+  // }
+
+
+  //   setTimeout((2,second) => {
+      
+  //   }, timeout);
+  //   roof();
+  
+
+  //  function sum(a,b){
+  //   return total = 100 + 100;
+
+  //  }
+
+  //  var funx = sum(10,20);
+       
+  //  console.log(funx)
+
+  // function fun(a,b){
+  //   return  (console.log(a+b))  
+  // }
+   
+  // let hello = name => `Hello, ${name}!`;
+
+  // console.log(hello('sohail')); // Hello, Brad!
+
+  //  let hello = name => `hello, ${name}!`;
+
+
+   let arrow = name => 'Hello', ${name}!`;
+    
+    console.log(hello('sohail')); hello,Bard!
+    
+
+
+
+
+
+
+
+
